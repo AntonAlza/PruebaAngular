e96f53f8f94816b9fbd1a21b731b942d19e817b7
AntonyAlza <aalzap@alicorp.com.pe>
2022-09-16 13:45:09 -0500

    Proyecto base

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { StudentComponent } from './components/student/student.component';
+import { ToolbarComponent } from './components/toolbar/toolbar.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    StudentComponent,
+    ToolbarComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule
